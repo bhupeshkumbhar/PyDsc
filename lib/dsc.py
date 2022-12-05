46e06da4ccecd6d1e3c61a5d325f0f7f61d2a44b
Bhupesh Kumbhar <bhupesh.kumbhar@gmail.com>
2022-12-05 21:07:16 +0530

    DSC Co-ordinates

--- lib/dsc.py
+++ lib/dsc.py
@@ -1,14 +1,25 @@
-import sys
+import sys,json,base64
 import datetime
 from endesive.pdf        import cms
 from cryptography.hazmat import backends
 from cryptography.hazmat.primitives.serialization import pkcs12
 
 def set_dsign(inp_data):
+    cfg_file = open('cfg/config.json')
+    cfg_json = json.load(cfg_file)
     inp_file = inp_data[0]
+    dsc_date = inp_data[1]
+    dsc_outp = inp_data[2]
+    dsc_ccod = inp_data[3]
+    dsc_cord = cfg_json["DSC_OUTP_LIST"][dsc_outp]
+    print(dsc_cord[1])
+    img_file = inp_file.replace("_inp","_img")
     out_file = inp_file.replace("_inp","_out")
     dsc_file = out_file.replace("inp/","out/")
     dsc_file = dsc_file.replace("_out","_signed")
+    dsc_path = cfg_json["DSC_CERT_LIST"][dsc_ccod]["DSC_CERT_PATH"]
+    dsc_pswd = cfg_json["DSC_CERT_LIST"][dsc_ccod]["DSC_CERT_PSWD"]
+    dsc_pswd = base64.b64decode(dsc_pswd)
     date = datetime.datetime.utcnow() - datetime.timedelta(hours=12)
     date = date.strftime("D:%Y%m%d%H%M%S+00'00'")
     dct = {
@@ -20,7 +31,7 @@ def set_dsign(inp_data):
         "sigfield": "Signature1",
         "auto_sigfield": True,
         "sigandcertify": True,
-        "signaturebox": (370, 16, 550, 80),
+        "signaturebox": (dsc_cord[1],dsc_cord[2],dsc_cord[3],dsc_cord[4]), #(370, 16, 550, 80),
         "signature": "",
         "signature_img": "",
         "contact": "",
@@ -29,9 +40,10 @@ def set_dsign(inp_data):
         "reason": "Digital Sign",
         "password": "Test@123",
     }
-    with open("cfg/dsc_1100.pfx", "rb") as fp:
+
+    with open(dsc_path, "rb") as fp:
         dsc_cert = pkcs12.load_key_and_certificates(
-            fp.read(), b"12345678", backends.default_backend()
+            fp.read(), dsc_pswd, backends.default_backend()
         )
 
     out_data = open(out_file, "rb").read()

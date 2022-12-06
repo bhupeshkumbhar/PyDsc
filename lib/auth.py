df9d66f6b160090c090fa77d7600066088cb1fa7
Bhupesh Kumbhar <bhupesh.kumbhar@gmail.com>
2022-12-06 06:35:16 +0530

    OTP Functionality Added

--- lib/auth.py
+++ lib/auth.py
@@ -1,6 +1,4 @@
-import requests
-from easygui import enterbox
-from easygui import msgbox
+import sys,requests
 
 def new_account():
     api_url = 'https://easy-authenticator.p.rapidapi.com/newAuthKey?'
@@ -10,14 +8,29 @@ def new_account():
     response = requests.request("POST",api_url,headers=api_hdr,params=api_qry)
     print(response.json())
 
-def auth_user():
-    token = enterbox("Please enter Token")
+def auth_user(inp_data):
+    usname = inp_data[0]
+    secret = inp_data[1]
+    gtoken = inp_data[2]
+
     api_url = 'https://easy-authenticator.p.rapidapi.com/verify'
-    api_qry = {"secretCode":"333K6O6QVP5SH7UR7JH7A6RGT3LZMWZB","token":token,"name":"bhupesh.kumbhar"}
+    api_qry = {"secretCode":secret,"token":gtoken,"name":usname}
     api_hdr = {"X-Rapidapi-Key":"472f53a83bmsh0945009f2fe0c7bp1d4da4jsn137f40e946b5","X-RapidAPI-Host": "easy-authenticator.p.rapidapi.com"}
 
-    response = requests.request("POST",api_url,headers=api_hdr,params=api_qry)
-    if response.json()["verify"]:
-        msgbox("One Time Password is Correct","Message","Continue")
+    api_rsp = requests.request("POST",api_url,headers=api_hdr,params=api_qry)
+
+    if api_rsp.json()["verify"]:
+        lv_resp = "True"
     else:
-        msgbox("One Time Password is inCorrect","Message","Continue")
+        lv_resp = "False"
+    
+    print(lv_resp)
+
+    with open("resp.txt", "w") as resp:
+        resp.write(lv_resp)
+
+
+
+
+
+
--- otp_auth.py
+++ otp_auth.py
@@ -1,3 +1,10 @@
+import sys
 from lib import auth
 
-auth.auth_user()
+if len(sys.argv) > 1:
+    inp_data = sys.argv[1]
+else:
+    inp_data = 'Bhupesh.Kumbhar,333K6O6QVP5SH7UR7JH7A6RGT3LZMWZB,8920392'
+
+inp_data = inp_data.split(",")
+auth.auth_user(inp_data)

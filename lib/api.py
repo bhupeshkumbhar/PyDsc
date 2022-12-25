a0d873daf18b5f69b9fec036b2c02fdfb223421f
Bhupesh Kumbhar <bhupesh.kumbhar@gmail.com>
2022-12-25 13:35:19 +0530

    add requirement.txt

--- lib/api.py
+++ lib/api.py
@@ -1,7 +1,9 @@
 import requests 
-
+import pandas as pd
+base_url = 'https://sandbox.surepass.io/'
 def company_details(inp_data):
-    api_qurl = 'https://sandbox.surepass.io/api/v1/corporate/company-details'
+    api_rurl = 'api/v1/corporate/company-details'
+    api_xurl = base_url + api_rurl
     api_auth = 'Bearer '+ 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY3MTQyOTQxMSwianRpIjoiNjY5MjMzNzctNzA0YS00M2Y5LTg3NTItZTliZjU1MzBmYWE2IiwidHlwZSI6ImFjY2VzcyIsImlkZW50aXR5IjoiZGV2LnplbmNvbkBzdXJlcGFzcy5pbyIsIm5iZiI6MTY3MTQyOTQxMSwiZXhwIjoxNjcxOTQ3ODExLCJ1c2VyX2NsYWltcyI6eyJzY29wZXMiOlsid2FsbGV0Il19fQ.fIdxKwX33625N1WNKaqvOVWrjpelK9r6XHdWJkUKtWw'
     api_head = {
         'Authorization': api_auth,
@@ -9,9 +11,9 @@ def company_details(inp_data):
     }   
     api_body = { "id_number": "U74999PN2017PTC168776"}
 
-    api_resp = requests.post(api_qurl,headers=api_head,json=api_body)
-
-    print(api_resp.text)
+    api_resp = requests.post(api_xurl,headers=api_head,json=api_body)
+    table = pd.read_json(api_resp.json(), typ='series')
+    print(table)
 
 inp_data={}
 company_details(inp_data)
